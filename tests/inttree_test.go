a14b337fb02c2ced9229a5d909316a8dcd98825f
Artur Grabowski <art@blahonga.org>
2016-11-17 11:57:08 +0100

    No more need for separate files here.

--- tests/inttree_test.go
+++ tests/inttree_test.go
@@ -2,6 +2,17 @@ package trees
 
 import "testing"
 
+//go:generate go run ../cmd/avlgen/main.go .
+
+type iV struct {
+	v     int
+	tlink tLink `avlgen:"iT"`
+}
+
+func (a *iV) cmp(b *iV) (bool, bool) {
+	return a.v == b.v, a.v < b.v
+}
+
 func TestIntsBasic(t *testing.T) {
 	tr := iT{}
 
@@ -14,6 +25,19 @@ func TestIntsBasic(t *testing.T) {
 			t.Errorf("%v != %v\n", i, v.v)
 		}
 	}
+}
+
+type iKV struct {
+	k, v int
+	tl   tl `avlgen:"ikvt,cmp:cmpiv,cmpval:cmpk(int),debug"`
+}
+
+func (a *iKV) cmpiv(b *iKV) (bool, bool) {
+	return a.k == b.k, a.k < b.k
+}
+
+func (a *iKV) cmpk(b int) (bool, bool) {
+	return a.k == b, a.k < b
 }
 
 func TestIntsLookupVal(t *testing.T) {
--- tests/stringtree_test.go
+++ tests/stringtree_test.go
@@ -5,6 +5,19 @@ import (
 	"testing"
 )
 
+type ss struct {
+	k, v string
+	tsl  tsl `avlgen:"sst,cmpval:cmpk(string)"`
+}
+
+func (a *ss) cmp(b *ss) (bool, bool) {
+	return a.k == b.k, a.k < b.k
+}
+
+func (a *ss) cmpk(b string) (bool, bool) {
+	return a.k == b, a.k < b
+}
+
 func BenchmarkSS1Mlinear(b *testing.B) {
 	const sz = 1000000
 

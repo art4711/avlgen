1d8cefdc74a7edb65f6e5820dac6e54cd2597bf6
Artur Grabowski <art@blahonga.org>
2016-11-24 10:45:32 +0100

    Use one of the trees here to test export.

--- tests/multi_test.go
+++ tests/multi_test.go
@@ -5,7 +5,7 @@ import "testing"
 type mt struct {
 	x, y int
 	mtlx mtlx `avlgen:"mtx,cmp:cmpx,no:last"`
-	mtly mtly `avlgen:"mty,cmp:cmpy,no:delete,no:first,no:last"`
+	mtly mtly `avlgen:"mty,cmp:cmpy,no:delete,no:first,no:last,export"`
 }
 
 func (a *mt) cmpx(b *mt) (bool, bool) {
@@ -24,14 +24,14 @@ func TestMultiTree(t *testing.T) {
 	for i := range vals {
 		m := &mt{x: vals[i], y: vals[len(vals)-1-i]}
 		tx.insert(m)
-		ty.insert(m)
+		ty.Insert(m)
 	}
 	for i := 1; i <= 7; i++ {
 		tx.delete(tx.lookup(&mt{x: 1 + ((i * 2) % 7)}))
 	}
 	for i := 1; i <= 7; i++ {
 		x := &mt{y: i}
-		if ty.lookup(x) == nil {
+		if ty.Lookup(x) == nil {
 			t.Error("!y")
 		}
 	}
